151268bb5d97ce05468405b4021e33c811a02093
Hossam Ragab <hossam.alasklany@gmail.com>
2022-12-05 10:33:01 +0200

    navbar css done!

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import { Component } from "react";
-import "../styles/NavbarStyles.css";
-import { MenuItems } from "./MenuItems";
-import { Link } from "react-router-dom";
+import React from 'react'
+import "./styles.css"
+import Navbar from "./components/Navbar"
+import { Router, Routes } from 'react-router-dom'
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar-items">
-        <h1 className="navbar-logo">Trippy</h1>
-        <ul className="nav-menu">
-          <li>
-            <Link>
-              <i class="fa-solid fa-house-user"></i>Home
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
+
+export default function App() {
+  return (
+    <div className='App'>
+        <Navbar />
+    </div>
+  )
 }
-export default Navbar;
+
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,37 @@
 import { Component } from "react";
-import "../styles/NavbarStyles.css";
+import "../styles/Navbar.css";
 import { Link } from "react-router-dom";
+import { MenuItems } from "./MenuItems" 
 
 class Navbar extends Component {
+    state = {clicked: false} ;
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+
     render() {
         return (
         <nav className="navbar-items">
             <h1 className="navbar-logo">Trippy</h1>
-            <ul className="nav-menu">
-            <li>
-                <Link>
-                <i class="fa-solid fa-house-user"></i>Home
-                </Link>
-            </li>
+
+            <div className="menu-icons" onClick={this.handleClick}>
+                <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+            </div>
+
+            <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+                {MenuItems.map((item, index) => {
+                    return(
+                        <li key={index}>
+                            <a className={item.cName} href={item.url}>
+                            <i className={item.icon}></i>{item.title}
+                            </a>
+                        </li>
+                    )
+                })}
+                <button className="btn-sign btn btn-outline-secondary">Sign Up</button>
             </ul>
+
         </nav>
         );
     }
